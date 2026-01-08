bd69f819bf3123e93a3a93236088ebfb8cee0ce1
张程(OrangeLan) <zhangcheng@hypergryph.com>
2026-01-08 19:44:58 +0800

    [新增脚本] 提交脚本翻新

--- scripts/commit.js
+++ scripts/commit.js
@@ -63,7 +63,7 @@ async function run() {
   try {
     log.yellow('Resetting staging area...');
     try {
-    //   execSync('git reset');
+      execSync('git reset');
     } catch (e) {}
 
     let files = await getChangedFiles();

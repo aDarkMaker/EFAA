a41141fd9a0fbe0fde3bf3119de949f5ff1a0a54
Zhuoran Yi <yizr_cnyali@qq.com>
2026-01-16 02:59:26 -0700

    Revert "[后端引擎] 增加 ok-script-boilerplate 模块"
    This reverts commit aa776b1d6169f6bbf1a80398efccaf5acdaaa5cd.

--- main_oks.py
+++
@@ -1,7 +0,0 @@
-import ok
-from resource.config import config
-
-if __name__ == '__main__':
-    config = config
-    ok = ok.OK(config)
-    ok.start()
--- main_oks_debug.py
+++
@@ -1,8 +0,0 @@
-import ok
-from resource.config import config
-
-if __name__ == '__main__':
-    config = config
-    config['debug'] = True
-    ok = ok.OK(config)
-    ok.start()

15ff8c85dc1fe876e7eb7adf8ead25fef6b70bdf
aDarkMaker <2261265112@qq.com>
2026-01-19 09:57:28 +0800

    [业务任务] 日常上线收取信用点

---
+++ backend/tasks/daily/credit_points.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import time
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+if BASE_DIR not in sys.path:
+    sys.path.insert(0, BASE_DIR)
+
+from utils.click.click import click, click_text
+from utils.keyboard.keyboard import keyevent
+
+def run_task(device_id="127.0.0.1:7555"):
+    print("[Task] Starting Credit Points task...")
+    
+    if click("shop", device_id=device_id):
+        time.sleep(0.2)
+    
+    if click_text("信用交易所", device_id=device_id):
+        time.sleep(0.2)
+    
+    if click_text("收取信用", device_id=device_id):
+        time.sleep(0.2)
+
+        keyevent("space", device_id=device_id)
+        time.sleep(0.2)
+
+        keyevent("esc", device_id=device_id)
+
+        print("[Task] Credit Points task completed.")
+
+    else:
+        print("[Task] Failed to do '收取信用'")
+
+if __name__ == "__main__":
+    run_task()

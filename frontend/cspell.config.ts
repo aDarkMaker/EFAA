2ae16ab0dfa8c96bf6ed983552ca5cd31d232179
homearchbishop <homearchbishop@foxmail.com>
2026-01-21 23:56:21 +0800

    chores: knip & cspell

---
+++ frontend/cspell.config.ts
@@ -0,0 +1,5 @@
+import { defineConfig } from 'cspell'
+
+export default defineConfig({
+  ignorePaths: ['node_modules', 'dist'],
+})
---
+++ frontend/knip.config.ts
@@ -0,0 +1,6 @@
+import type { KnipConfig } from 'knip'
+
+export default {
+  ignore: [
+  ],
+} satisfies KnipConfig

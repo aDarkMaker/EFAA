304406d730c205ee1ecf9d04d41a190543330d08
aDarkMaker <2261265112@qq.com>
2026-01-22 19:06:35 +0800

    [前端开发] get_port

--- frontend/src/common/request/index.ts
+++ frontend/src/common/request/index.ts
@@ -3,9 +3,22 @@ import { throwErrorMiddleware } from '@/common/request/middlewares/throw-error'
 
 import RequestHub from '../../core/request/request-hub'
 
-const baseUrl = 'http://localhost:8000'
+const getBaseUrl = async () => {
+  // 如果在 pywebview 环境中
+  if (window.pywebview && window.pywebview.api && window.pywebview.api.get_api_port) {
+    try {
+      const port = await window.pywebview.api.get_api_port()
+      return `http://127.0.0.1:${port}`
+    } catch (e) {
+      console.error('Failed to get port from pywebview', e)
+    }
+  }
+  // 默认回退（开发环境或获取失敗）
+  return 'http://127.0.0.1:8000'
+}
 
-export const requestDefault = () => {
+export const requestDefault = async () => {
+  const baseUrl = await getBaseUrl()
   const request = new RequestHub()
   request
     .use(throwErrorMiddleware)
